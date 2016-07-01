75ebe717b8f9a9bb552b005f62f46cfafb214119
Alejandro Cortes <Alejandro Cortes>
2016-06-30 22:23:54 -0400

    added survey html and edited other parts of the project

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,5 @@
 var friends = [{
-	name: "Joey",
+	name: "LL Cool J.",
 	photo: "https://www.facebook.com/profile.php?id=100005729641147",q
 	scores: [
 		'1',
@@ -15,7 +15,7 @@ var friends = [{
 	]
 },
 {
-	name: "John",
+	name: "Obama",
 	photo: "https://scontent.ftpa1-2.fna.fbcdn.net/v/t1.0-9/13321835_749603687811_749594492205669389_n.jpg?oh=20556b0136c73f2a1d25dc44aad14db3&oe=57FDC732"
 	scores: [
 		'1',
@@ -31,7 +31,7 @@ var friends = [{
 	]
 },
 {
-	name: "Rob",
+	name: "Rob Stark",
 	photo: "https://scontent.ftpa1-2.fna.fbcdn.net/t31.0-8/11055363_10154428411358312_2964402085548907340_o.jpg",
 	scores: [
 		'4',
