57fe0c005d8239bf31b778e8f496803ac23156df
Alejandro Cortes <Alejandro Cortes>
2016-07-02 11:13:39 -0400

    working on api-routes.

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,7 +1,37 @@
+var friends = require('../data/friend-data.js');
+var path = require('path');
+
+
+module.exports = function(app) {
+
 // More Routes
 // =============================================================
 
-app.get('/friends', function(req, res){
-	//res.send("Welcome to the Star Wars Page!")
+app.get('/api/friends', function(req, res){
+	// res info to from friends.js
 	res.sendFile(path.join(__dirname + '/friends.js.'));
-})
+});
+
+app.post('/api/friends', function(req, res) {
+
+	var body =req.body;
+
+	var totalScore = 100;
+	var friendIndex = 0;
+
+	// loop through friends
+	for (var = i; i < friends.length; i++) {
+
+		var diff = compare(friends[i].scores, body.scores)
+
+		if (i == 0) {
+			totalScore = i;
+			friendIndex = diff;
+		} if (diff < totalScore) {
+			friendIndex = i;
+			totalScore = diff;
+		}
+
+	}
+	res.json(friends[friendIndex]);
+}
